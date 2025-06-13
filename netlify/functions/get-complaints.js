a706e038542988e4fb4f3ad5ba860a290d96b5ec
fglema <francoglema@yahoo.com>
2025-06-12 21:23:32 -0500

    Use global fetch in get-complaints function and remove node-fetch

--- netlify/functions/get-complaints.js
+++ netlify/functions/get-complaints.js
@@ -1,19 +1,31 @@
-const fetch = require('node-fetch');
+exports.handler = async () => {
+  try {
+    const FORM_ID = 'complaints';
+    const TOKEN   = process.env.NETLIFY_AUTH_TOKEN;
 
-exports.handler = async (event, context) => {
-  const FORM_ID = 'complaints';
-  const AUTH_TOKEN = process.env.NETLIFY_AUTH_TOKEN;
-  const resp = await fetch(
-    \`https://api.netlify.com/api/v1/forms/\${FORM_ID}/submissions\`,
-    { headers: { Authorization: \`Bearer \${AUTH_TOKEN}\` } }
-  );
-  if (!resp.ok) {
-    return { statusCode: resp.status, body: await resp.text() };
+    // Fetch Netlify Forms submissions
+    const res = await fetch(
+      `https://api.netlify.com/api/v1/forms/${FORM_ID}/submissions`,
+      {
+        headers: { Authorization: `Bearer ${TOKEN}` }
+      }
+    );
+    if (!res.ok) {
+      return {
+        statusCode: res.status,
+        body: `Error fetching submissions: ${res.statusText}`
+      };
+    }
+    const submissions = await res.json();
+    return {
+      statusCode: 200,
+      headers: { 'Access-Control-Allow-Origin': '*' },
+      body: JSON.stringify(submissions)
+    };
+  } catch (err) {
+    return {
+      statusCode: 500,
+      body: `Function error: ${err.message}`
+    };
   }
-  const submissions = await resp.json();
-  return {
-    statusCode: 200,
-    headers: { 'Access-Control-Allow-Origin': '*' },
-    body: JSON.stringify(submissions),
-  };
 };
